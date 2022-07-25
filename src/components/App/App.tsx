5909a2e70d746f0c2010cc88eaf66eb7f1102fb7
Fpsska <fpsska1337@gmail.com>
2022-07-25 16:23:47 +0300

    init: initial commit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import '../../assets/style.scss';
+import './App.css';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      test
+    </div>
+  );
+};
+
+export default App;
