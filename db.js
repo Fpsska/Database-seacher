d6482ee69306547d8edaa97413e78aa7cd1def10
Fpsska <fpsska1337@gmail.com>
2022-07-31 15:52:14 +0300

    feat: install dotenv npm package & create .env file with db configuration, use in db.js

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
+
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user: 'jwagicgk',
-    password: '9dkNX4FaZTp9FaypHqAjktVntKTQR3Y2',
-    host: 'manny.db.elephantsql.com',
-    port: 5432,
-    database: 'jwagicgk'
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
 })
 
 module.exports = pool;
