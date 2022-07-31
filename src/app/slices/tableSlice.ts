81c3f02e95f7fb2880eb6487f3290b9355cb1e33
Fpsska <fpsska1337@gmail.com>
2022-07-31 15:55:11 +0300

    refactor: change fetch URL for fetchTableData func

--- src/app/slices/tableSlice.ts
+++ src/app/slices/tableSlice.ts
@@ -8,7 +8,7 @@ export const fetchTableData = createAsyncThunk(
     'tableSlice/fetchTableData',
     async (_, { rejectWithValue }) => {
         try {
-            const response = await fetch('https://vercel-backend-test-jq1q7qnim-fpsska.vercel.app/api/data');
+            const response = await fetch('https://welbex-task-backend.vercel.app/api/data');
 
             if (!response.ok) {
                 throw new Error('Error from response');
