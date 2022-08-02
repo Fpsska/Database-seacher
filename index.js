41d7a37a0dc098b446f47a436d1af0b2ffcf649a
Fpsska <fpsska1337@gmail.com>
2022-08-02 11:27:02 +0300

    refactor: add PORT configuration in .env file & change path to index.js file in start script in package.json

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.get('/', (req, res) => {
 
 
 // connection
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
